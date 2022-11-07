66e0310fc605fac4e657af6da809539a6a7abac9
Malevych Yurii <yuramvm1@gmail.com>
2022-11-07 18:35:36 +0200

    add twig, reformatting index.html to twig templates

--- index.php
+++ index.php
@@ -3,19 +3,28 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$view = new \Twig\Environment($loader);
 
 // Create app
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About page!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Yurii'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
