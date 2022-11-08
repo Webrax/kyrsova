b1983413fbbf730eb859688e76b1082edda4eefd
Malevych Yurii <yuramvm1@gmail.com>
2022-11-08 10:44:44 +0200

    Designing the post page and URL (software management). Writing DB connection logic

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php /* an associative array that includes 3 values for the database*/
+
+return [
+  'dsn' => 'mysql:host=127.0.0.1; dbname=site_php',
+  'username' => 'root',
+  'password' => '',
+];
--- index.php
+++ index.php
@@ -8,9 +8,30 @@ use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
 
+ /*
+ logic for displaying templates
+ */
+
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $view = new \Twig\Environment($loader);
 
+/* variables for the database connection */
+$config = include 'config/database.php';
+$dsn = $config['dsn'];
+$username = $config['username'];
+$password = $config['password'];
+
+/* uses the setAttribute method to connect */
+try { /* I use try catch to prevent any exceptional situations when connecting the DB */
+$connection = new PDO($dsn, $username, $password);
+/* error type */
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/* the logic is how I will load the data when I use select, FETCH_ASSOS is an associative array */
+$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+    echo 'Database error: ' . $exception->getMessage();
+    die();
+}
 // Create app
 $app = AppFactory::create();
 
@@ -27,5 +48,15 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
     $response->getBody()->write($body);
     return $response;
 });
+
+/* route for post URL (normal link)*/
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('post.twig', [
+        'url_key' => $args['url_key']
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
 
 $app->run();
